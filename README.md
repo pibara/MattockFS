8dcaf1aef65cfcc0569c1ecacfe91bb51da39cd6
Rob Meijer <pibara@gmail.com>
2015-12-14 06:35:05 +0100

    moved pymattockfs stuff to own gir repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# mattock-dissertation
-For now just a place for the ongoing work on my UCD FCCI MSc dissertation.
--- ocfa/step1/stripall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./strip2.py ocfasql1.gz stripped1.gz
-./strip2.py ocfasql2.gz stripped2.gz
-./strip2.py ocfasql3.gz stripped3.gz
-./strip2.py ocfasql4.gz stripped4.gz
-./strip2.py ocfasql5.gz stripped5.gz
